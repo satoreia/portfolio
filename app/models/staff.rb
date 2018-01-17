7d8fdd240aaf7aeae6c4eb1437a32056f097684a
佐藤鈴愛 <reia@satousuzuainoMacBook-Pro.local>
2018-01-17 17:51:14 +0900

    devise:views作成

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,6 @@
 class Staff < ApplicationRecord
-	has_many :blogs
-	has_many :news
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
