d81fcbdbd74f69e3bded5a90037090c5f0ba9db1
佐藤鈴愛 <reia@satousuzuainoMacBook-Pro.local>
2018-01-17 20:09:15 +0900

    login画面完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117083545) do
+ActiveRecord::Schema.define(version: 20180117104936) do
 
   create_table "blogs", force: :cascade do |t|
     t.integer "staff_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20180117083545) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_staffs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_staffs_on_reset_password_token", unique: true
   end
