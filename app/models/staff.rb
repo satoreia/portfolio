1652d128528f1b2f88755f3689bd3765765f3174
佐藤鈴愛 <reia@satousuzuainoMacBook-Pro.local>
2018-01-26 13:27:56 +0900

    has_many訂正

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -3,5 +3,10 @@ class Staff < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :news, :blogs, :abouts, :questions, :dropins, :packs
+  has_many :news, :dependent => :destroy
+  has_many :blogs
+  has_many :abouts
+  has_many :questions
+  has_many :dropins
+  has_many :packs
 end
